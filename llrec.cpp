56cdca2b0c6c061d402f83959ec79f8d4eb2f468
MJ Deus <mdeus@usc.edu>
2025-10-21 17:11:19 -0700

    testing out LL pivot from part 1

--- llrec.cpp
+++ llrec.cpp
@@ -4,4 +4,67 @@
 // Provide your implementation of llpivot below
 //*********************************************
 
+// included here to avoid switching tabs back and forth, ignore for graders
+/**
+ * Given a linked list pointed to by head, creates two lists
+ * where all values less than or equal to the pivot value are
+ * placed in a linked list whose head will be pointed to by
+ * smaller and all values greater than the pivot
+ * value are placed in a linked list whose head will be pointed to
+ * by larger.  The input list pointed to by head should be empty
+ * upon return and head set to NULL (i.e. we are not making copies)
+ * in the smaller and larger lists but simply moving Nodes out of
+ * the input list and into the two other lists.
+ * 
+ * ==============================================================
+ * MUST RUN IN O(n) where n is the number of nodes in the input list
+ * ==============================================================
+ *
+ * @pre: smaller and larger may containing garbage (do NOT have
+ *       to be NULL)
+ *
+ * @param[inout] head
+ *   Reference to the head pointer to the input list.
+ *   Should be set to NULL upon return
+ * @param[out] smaller
+ *   Reference to a head pointer for the list of nodes with values
+ *   less than or equal to the pivot
+ * @param[out] larger
+ *   Reference to a head pointer for the list of nodes with values
+ *   greater than the pivot
+ * @param[in] pivot
+ *   Pivot value
+ *
+ */
+void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot){
+  // base case when list is empty
+  if(head == nullptr){
+    return;
+  }
 
+  // might need this? not super sure
+  // make Node ptr to maintain list order
+  //Node* tmp = head;
+
+  // place current item in correct list
+  if(head->val =< pivot){
+    if(smaller == nullptr){
+      smaller = head;
+    }
+    else{
+      smaller->next = head;
+    }
+  }
+  else if (head->val > pivot){
+    if(larger == nullptr){
+      larger = head;
+    }
+    else{
+      larger->next = head;
+    }
+  }
+
+  // done processing, time to go back 
+  return llpivot(head->next, smaller, larger, pivot);
+  head = nullptr;
+}
