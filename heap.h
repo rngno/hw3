7e4b4701e8bbdf08ab3ac6dcd2e9830bdc3f60b3
MJ Deus <mdeus@usc.edu>
2025-10-22 21:29:33 -0700

    heap most fxns implemented, testing now

--- heap.h
+++ heap.h
@@ -16,13 +16,13 @@ public:
    *          as an argument and returns a bool if the first argument has
    *          priority over the second.
    */
-  Heap(int m=2, PComparator c = PComparator());
+  Heap(int m=2, PComparator c = PComparator()) : m_(m), comp_(c) {};
 
   /**
   * @brief Destroy the Heap object
   * 
   */
-  ~Heap();
+  ~Heap() = default;
 
   /**
    * @brief Push an item to the heap
@@ -63,13 +63,22 @@ public:
 private:
   /// Add whatever helper functions and data members you need below
   std::vector<T> heap;
-
+  int m_;
+  PComparator comp_;
+  
+  // have to use two different member fxns, can't figure out how to make it in one
+  void heapifyPush(size_t idx);
+  void heapifyPop(size_t idx);
 
 
 };
 
 // Add implementation of member functions here
-
+template<typename T, typename PComparator>
+void Heap<T, PComparator>::push(const T& item){
+  heap.push_back(item); // slap item on back and we can sort after
+  heapifyPush(heap.size()-1); // need special member fxn for this operation
+}
 
 // We will start top() for you to handle the case of 
 // calling top on an empty heap
@@ -105,11 +114,65 @@ void Heap<T,PComparator>::pop()
     throw std::underflow_error("empty heap");
 
   }
-  heap.erase(heap.begin());
+  
+  // follow heap algorithm and slap worst/bottom to the top after popping the top
+  heap[0] = heap.back();
+  heap.pop_back(); // can use vector fxn here 
 
+  if(!empty()){
+    heapifyPop(0); // start at top and work our way down to maintain heap prop
+  }
 
 }
 
+template<typename T, typename PComparator>
+bool Heap<T, PComparator>::empty() const{
+  return heap.empty(); // easy to use vector
+}
+
+template<typename T, typename PComparator>
+size_t Heap<T, PComparator>::size() const{
+  return heap.size(); // easy to use vector 
+}
+
+template<typename T, typename PComparator>
+void Heap<T, PComparator>::heapifyPush(){
+  // just gonna walk through the whole thing
+  while(idx > 0){
+    size_t parent = (idx-1)/m_;
+    if(comp_(heap[idx], heap[parent])){
+      std::swap(heap[idx], heap[parent]); // swap if idx is better than its parent
+      idx = parent;
+    }
+    else{
+      break; // catch when child is not better, then we can stop
+    }
+  }
+}
+
+template<typename T, typename PComparator>
+void Heap<T, PComparator>::heapifyPop(){
+  size_t first = m_ * idx+1;
+  while(first < heap.size()){
+    size_t best = first;
+    for(size_t i = 1; i<m_ && first+i<heap.size(); ++i){
+      size_t curr = first + i;
+      if(comp_(heap[curr], heap[best])){
+        best = curr;
+      }
+    }
+
+    // mostly the same as heapifyPush but i can't abstract this i don't think?
+    if(comp_(heap[best], heap[idx])){
+      std::swap(heap[idx], heap[best]); // swap if idx is better than best
+      idx = best;
+      first = m_*idx+1; // walk forward
+    }
+    else{
+      break; // catch when child is not better, then we can stop
+    }
+  }
+}
 
 
 #endif
